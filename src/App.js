4965eb7a9de7d203f50abea2db78297821807aed
“Brenno <dev.brennoeudes@gmail.com>
2024-03-24 19:29:51 -0300

    gallows component creation, and importation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hangman from "./Hangman.js";
 import Header from "./Header.js";
+import Gallows from "./Gallows.js";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Gallows/>
       <Hangman />
     </>
   );
---
+++ src/Gallows.js
@@ -0,0 +1,7 @@
+export default function Gallows() {
+  return (
+    <>
+      <div className="gallows"></div>
+    </>
+  );
+}
