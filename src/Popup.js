fa2d24de4a1f8bc01efb92cc20599414be9443b2
“Brenno <dev.brennoeudes@gmail.com>
2024-03-24 22:09:52 -0300

    popup-wrapper

--- src/Popup.js
+++ src/Popup.js
@@ -3,11 +3,15 @@ const Popup = ({ status, word, reset }) => {
     return null;
   } else {
     return (
-      <div className="popup">
-        <p>{status}!</p>
-        <p>The word was: {word}.</p>
+      <div className="popup-wrapper">
+        <div className="popup">
+          <p>{status}!</p>
+          <p>The word was: {word}.</p>
 
-        <button id="reset-button" onClick={reset}>Play Again</button>
+          <button id="reset-button" onClick={reset}>
+            Play Again
+          </button>
+        </div>
       </div>
     );
   }
