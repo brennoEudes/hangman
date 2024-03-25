081213e0485d4352183fbf4f041dacda3d401854
“Brenno <dev.brennoeudes@gmail.com>
2024-03-25 11:24:00 -0300

    animal emoji

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,9 @@ export default function Header() {
   return (
     <>
       <div className="header">
-        <h1><span>Hangman</span>imals🌿</h1>
+        <h1>
+          <span>Hangman</span>imals 🐯
+        </h1>
         <p>Challenge your instincts guessing the animal name!</p>
       </div>
     </>
