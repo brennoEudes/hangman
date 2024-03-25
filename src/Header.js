b35b1b61c7fcbf6fce49ca4c6f740a74a951eef6
“Brenno <dev.brennoeudes@gmail.com>
2024-03-25 10:46:21 -0300

    game naming adjust

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <>
       <div className="header">
-        <h1>Hangman Nature🌿</h1>
+        <h1><span>Hangman</span>imals🌿</h1>
         <p>Challenge your instincts guessing the animal name!</p>
       </div>
     </>
