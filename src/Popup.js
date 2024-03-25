991645abc4bcbb4d1208e613b4b2a6587c7d1376
“Brenno <dev.brennoeudes@gmail.com>
2024-03-25 11:10:14 -0300

    popup whats app share adjust

--- src/Popup.js
+++ src/Popup.js
@@ -5,13 +5,18 @@ const Popup = ({ status, word, reset }) => {
     return (
       <div className="popup-wrapper">
         <div className="popup">
-          <p>{status}!</p>
-          <p>The word was: {word}.</p>
+          <p className="status-popup">{status}!</p>
+          <p className="status-word">The word was: {word}.</p>
 
           <button id="reset-button" onClick={reset}>
             Play Again
           </button>
-          {/* <a href="https://wa.me/?text=I%20liked%20Hangman%20Nature%20game%20and%20I%20invite%20you!%20Try%20it%20now!" alt="What App link invite"></a> */}
+          <a
+            href="https://wa.me/?text=Hi!%20I%20liked%20HangmAnimals%20game%20and%20I%20invite%20you!%20Try%20it%20now!"
+            alt="What App link invite"
+          >
+            Invite your friends
+          </a>
         </div>
       </div>
     );
